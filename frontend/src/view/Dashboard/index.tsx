61865e1b85c9f0343a5e8c5f8720266f061ecc21
Fernando Butzke <fernandobutzke1@gmail.com>
2023-11-15 10:09:08 -0300

    feat: Adding icons + First styles of Dashboard

--- frontend/src/view/Dashboard/index.tsx
+++ frontend/src/view/Dashboard/index.tsx
@@ -1,16 +1,23 @@
 import { Logo } from "../components/Logo";
 import { UserMenu } from "../components/UserMenu";
+import { Accounts } from "./components/Accounts";
+import { Transactions } from "./components/Transactions";
 
 export function Dashboard() {
   return (
-    <div className="w-full h-full px-8 pb-8 pt-6">
+    <div className="w-full h-full p-4 md:px-8 md:pb-8 md:pt-6 flex flex-col gap-4">
       <header className="flex justify-between items-center">
         <Logo className="h-6 text-teal-900" />
         <UserMenu />
       </header>
 
-      <main>
-        content
+      <main className="flex-1 flex gap-4 flex-col md:flex-row">
+        <div className="w-full md:w-1/2 ">
+          <Accounts />
+        </div>
+        <div className="w-full md:w-1/2 ">
+          <Transactions />
+        </div>
       </main>
     </div>
   );
