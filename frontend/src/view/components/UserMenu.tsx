d65862a42a8cedc38f0e57bbe66c81c62ae74e35
Fernando Butzke <fernandobutzke1@gmail.com>
2023-11-28 12:33:11 -0300

    feat: Styled Radix-UI DropdownMenu component

--- frontend/src/view/components/UserMenu.tsx
+++ frontend/src/view/components/UserMenu.tsx
@@ -1,7 +1,21 @@
+import { ExitIcon } from "@radix-ui/react-icons";
+import { DropdownMenu } from './DropdownMenu';
+
 export function UserMenu() {
   return (
-    <div className="bg-teal-50 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100">
-      <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">FB</span>
-    </div>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <div className="bg-teal-50 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100">
+          <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">FB</span>
+        </div>
+      </DropdownMenu.Trigger>
+
+        <DropdownMenu.Content className="w-32">
+          <DropdownMenu.Item className="flex items-center justify-between">
+            Sair
+            <ExitIcon className="w-6" />
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+    </DropdownMenu.Root>
   );
 }
